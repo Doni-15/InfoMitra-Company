f4b7eb1f61b3308b1629d4041816726eb2964992
Doni Rivaldo Simamora <donisimamora15@gmail.com>
2025-12-10 09:59:27 +0700

    Me : It works on my computer. Other : Great… but it doesn’t work anywhere else.

--- InfoMitra-Frontend/src/Components/component/main-layout.jsx
+++ InfoMitra-Frontend/src/Components/component/main-layout.jsx
@@ -5,7 +5,7 @@ export function MainLayout({ user, setUser }) {
     return (
         <>
             <Header user={user} setUser={setUser} /> 
-            <main className="flex flex-1 pt-13 min-h-[90vh] flex-col overflow-hidden z-1">
+            <main className="flex flex-1 pt-10 min-h-[90vh] flex-col overflow-hidden z-1">
                 <Outlet />
             </main>
             <Footer />
